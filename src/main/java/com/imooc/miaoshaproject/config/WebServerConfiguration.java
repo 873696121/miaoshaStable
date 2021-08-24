08046db93c282ddb7f029dcf05f55183c51b72ff
hong <873696121@qq.com>
2021-08-24 23:11:40 +0800

    tomcat定制化配置

---
+++ src/main/java/com/imooc/miaoshaproject/config/WebServerConfiguration.java
@@ -0,0 +1,32 @@
+package com.imooc.miaoshaproject.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ProtocolHandler;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+/***********************************************************                                          *
+ * Time: 2021/8/24
+ * Author: HuHong
+ * Desc:
+ ***********************************************************/
+
+@Component
+public class WebServerConfiguration implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        ((TomcatServletWebServerFactory)factory).addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+                // 定制化keeplivetimeout 30秒
+                protocol.setKeepAliveTimeout(300000);
+                protocol.setMaxKeepAliveRequests(10000);
+            }
+        });
+    }
+}
